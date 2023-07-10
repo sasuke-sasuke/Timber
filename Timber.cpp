211770596c9bbca59959033a17a67b030409840d
Jeof Petty <jeofpetty@icloud.com>
2023-07-09 23:03:04 -0400

    added sprite and texture background and display to window

--- Timber.cpp
+++ Timber.cpp
@@ -7,6 +7,16 @@ int main()
 	VideoMode vm(1920, 1080);
 	// Create and open a window for the game
 	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+	// Create a texture to hold a graphic on the GPU
+	Texture textureBackground;
+	// Load a graphic into the texture
+	textureBackground.loadFromFile("graphics/background.png");
+	// Create a sprite
+	Sprite spriteBackground;
+	// Attach the texture to the sprite
+	spriteBackground.setTexture(textureBackground);
+	// Set the spriteBackground to cover the screen
+	spriteBackground.setPosition(0, 0);
 
 	while (window.isOpen())
 	{
@@ -32,6 +42,7 @@ int main()
 		// Clear everything from the last frame
 		window.clear();
 		// Draw our game scene here
+		window.draw(spriteBackground);
 		// Show everything we just drew
 		window.display();
 	}
