4d4e14b62930d14940cdbd049a8752cb7396852a
Jeof Petty <jeofpetty@icloud.com>
2023-07-10 16:04:30 -0400

    added tree, clouds, bee texture and sprite.. Draw to window

--- Timber.cpp
+++ Timber.cpp
@@ -17,6 +17,49 @@ int main()
 	spriteBackground.setTexture(textureBackground);
 	// Set the spriteBackground to cover the screen
 	spriteBackground.setPosition(0, 0);
+	
+	// Make a tree sprite
+	Texture textureTree;
+	textureTree.loadFromFile("graphics/tree.png");
+	Sprite spriteTree;
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(810, 0);
+	
+	// Prepare the bee
+	Texture textureBee;
+	textureBee.loadFromFile("graphics/bee.png");
+	Sprite spriteBee;
+	spriteBee.setTexture(textureBee);
+	spriteBee.setPosition(0, 800);
+	// Is the bee currently moving?
+	bool beeActive = false;
+	// How fast the bee flies
+	float beeSpeed = 0.0f;
+
+	// make 3 cloud sprites from 1 texture
+	Texture textureCloud;
+	// Load 1 new texture
+	textureCloud.loadFromFile("graphics/cloud.png");
+	// 3 New sprites with the same texture
+	Sprite spriteCloud1;
+	Sprite spriteCloud2;
+	Sprite spriteCloud3;
+	spriteCloud1.setTexture(textureCloud);
+	spriteCloud2.setTexture(textureCloud);
+	spriteCloud3.setTexture(textureCloud);
+	// Position the clouds on the left of the screen
+	// at different heights
+	spriteCloud1.setPosition(0, 0);
+	spriteCloud2.setPosition(0, 250);
+	spriteCloud3.setPosition(0, 500);
+	// Are the clouds currently on screen
+	bool cloud1Active = false;
+	bool cloud2Active = false;
+	bool cloud3Active = false;
+	// How fast is each cloud
+	float cloud1Speed = 0.0f;
+	float cloud2Speed = 0.0f;
+	float cloud3Speed = 0.0f;
 
 	while (window.isOpen())
 	{
@@ -43,6 +86,14 @@ int main()
 		window.clear();
 		// Draw our game scene here
 		window.draw(spriteBackground);
+		// Draw the clouds
+		window.draw(spriteCloud1);
+		window.draw(spriteCloud2);
+		window.draw(spriteCloud3);
+		// Draw the trees
+		window.draw(spriteTree);
+		// Draw the bee
+		window.draw(spriteBee);
 		// Show everything we just drew
 		window.display();
 	}
