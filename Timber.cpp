d1963740a455347d9551f096f39aefaf975339c8
Jeof Petty <jeofpetty@icloud.com>
2023-07-10 23:36:55 -0400

    added cloud and bee animations

--- Timber.cpp
+++ Timber.cpp
@@ -61,6 +61,10 @@ int main()
 	float cloud2Speed = 0.0f;
 	float cloud3Speed = 0.0f;
 
+	// Variables to control time
+	Clock clock;
+
+
 	while (window.isOpen())
 	{
 		/*
@@ -77,6 +81,106 @@ int main()
 		Update the scene
 		****************************************
 		*/
+		// Measure time
+		Time dt = clock.restart();
+		// Setup bee
+		if (!beeActive)
+		{
+			// How fast is the bee
+			srand((int)time(0));
+			beeSpeed = (rand() % 200) + 200;
+			// How high is the bee
+			srand((int)time(0) * 10);
+			float height = (rand() % 500) + 500;
+			spriteBee.setPosition(2000, height);
+			beeActive = true;
+		}
+		else
+			// Move the bee
+		{
+			spriteBee.setPosition(
+				spriteBee.getPosition().x - 
+				(beeSpeed * dt.asSeconds()),
+				spriteBee.getPosition().y);
+			// Has the bee reached the left-hand edge of screen?
+			if (spriteBee.getPosition().x < -100)
+			{
+				// Set up ready to be a whole new bee next frame
+				beeActive = false;
+			}
+		}
+		// Manage the clouds
+		// Cloud 1
+		if (!cloud1Active)
+		{
+			// How fast is the cloud
+			srand((int)time(0) * 10);
+			cloud1Speed = (rand() % 200);
+			// How high is the cloud
+			srand((int)time(0) * 10);
+			float height = (rand() % 150);
+			spriteCloud1.setPosition(-200, height);
+			cloud1Active = true;
+		}
+		else
+		{
+			spriteCloud1.setPosition(
+				spriteCloud1.getPosition().x +
+				(cloud1Speed * dt.asSeconds()),
+				spriteCloud1.getPosition().y);
+			// Has the clooud reached the right hand edge of the screen
+			if (spriteCloud1.getPosition().x > 1920)
+			{
+				// Set up cloud ready to be whole new cloud next frame
+				cloud1Active = false;
+			}
+		}
+		if (!cloud2Active)
+		{
+			// Speed of cloud
+			srand((int)time(0) * 20);
+			cloud2Speed = (rand() % 200);
+			// Height of cloud
+			srand((int)time(0) * 20);
+			float height = (rand() % 300) - 150;
+			spriteCloud2.setPosition(-200, height);
+			cloud2Active = true;
+		}
+		else
+		{
+			spriteCloud2.setPosition(
+				spriteCloud2.getPosition().x +
+				(cloud2Speed * dt.asSeconds()),
+				spriteCloud2.getPosition().y);
+			// Cloud reached right edge?
+			if (spriteCloud2.getPosition().x > 1920)
+			{
+				cloud2Active = false;
+			}
+		}
+		if (!cloud3Active)
+		{
+			// Speed of cloud
+			srand((int)time(0) * 30);
+			cloud3Speed = (rand() % 200);
+			// Height of cloud
+			srand((int)time(0) * 30);
+			float height = (rand() % 450) - 150;
+			spriteCloud3.setPosition(-200, height);
+			cloud3Active = true;
+		}
+		else
+		{
+			spriteCloud3.setPosition(
+				spriteCloud3.getPosition().x +
+				(cloud3Speed * dt.asSeconds()),
+				spriteCloud3.getPosition().y);
+			// Cloud reached right edge?
+			if (spriteCloud3.getPosition().x > 1920)
+			{
+				cloud3Active = false;
+			}
+		}
 		/*
 		****************************************
 		Draw the scene
